335a95009efabd8b9dd5d6757a12eba6efbcc9e4
Oumayma-cherif <oumayma.cherif@esprit.tn>
2021-04-25 04:12:26 +0100

    4 5 6 test

--- views/back/modifierCategorie.php
+++ views/back/modifierCategorie.php
@@ -167,9 +167,7 @@ font-size: 16px;"> Last access : 30 May 2014 &nbsp; <a href="login.html" class="
                     result = confirm("Are you sure you want to submit this form ?");
                     if (result) {
                         $('#form').submit();
-                        //why use jquery? so oldskool  idkk isnt mine
-                        //  good girl
-                        // document.querySelector("#form").submit(); EZ NO JQUERY NOTHING
+                        
                     }
                 }
             </script>
